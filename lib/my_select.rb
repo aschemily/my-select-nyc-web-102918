69d28e1b45946ac2edcca5a61cb8041647181306
Emily Asch <e.asch@me.com>
2018-10-15 18:19:20 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,4 +3,10 @@ def my_select(collection)
  count = 0 
  result = []
  while count < collection.size
+ if yield(array[count]) == true 
+   result.push(array[count])
+ end
+ count += 1 
+ end
+ result 
 end
